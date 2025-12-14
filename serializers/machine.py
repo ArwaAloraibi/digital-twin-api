926ffcc7d08412a46dd269f982c7333019a96944
Arwa Aloraibi <arwa7oraibi@gmail.com>
2025-12-14 22:58:16 +0300

    edit the sensor_data serializer

--- serializers/machine.py
+++ serializers/machine.py
@@ -3,7 +3,7 @@ from typing import Optional, List
 from .sensor_data import SensorDataSchema
 
 class machineSchema(BaseModel):
-  id: Optional[int] = True # This makes sure you don't have to explicitly add an id when sending json data
+  machine_id: Optional[int] = True # This makes sure you don't have to explicitly add an id when sending json data
   status: str  
   sensorsData: List[SensorDataSchema] = []
 
--- serializers/sensor_data.py
+++ serializers/sensor_data.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
 
 class SensorDataSchema(BaseModel):
-  id: int
-  content: str
+    machine_id: int
+    temperature: float
+    power_consumption_kw: float
+    network_latency_ms: float
+    error_rate_pct: float
+    efficiency_status: str 
 
-  class Config:
-    orm_mode = True
+    class Config:
+        orm_mode = True
